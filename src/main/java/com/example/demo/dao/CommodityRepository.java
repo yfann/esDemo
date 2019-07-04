6ce93c7ff87a034968fb8c976cf4643ad2bd380e
Fan <fan.yu@philips.com>
2019-07-04 23:37:00 +0800

    update

--- src/main/java/com/example/demo/dao/CommodityRepository.java
+++ src/main/java/com/example/demo/dao/CommodityRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.dao;
 
 import com.example.demo.entity.Commodity;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Component
 public interface CommodityRepository extends ElasticsearchRepository<Commodity, String> {
 
 }
